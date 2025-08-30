ddcc92c8686d90e93d0bcfe7cba24f24e40e7b8f
Amirhoseain Khodagholizadeh <99099321+amirhoseain-khodagholizadeh-web@users.noreply.github.com>
2025-08-30 13:26:35 +0330

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,6 @@ const playLineDot = document.querySelector(".play>.line>div>div>span")
 const startTimeList = document.querySelectorAll(".play>.line>.start>span")
 const endTimeList = document.querySelectorAll(".play>.line>.end>span")
 
-
-
 let listNum = 0
 
 
@@ -68,11 +66,23 @@ const musicList = [{
     }
 
 ]
-window.addEventListener("load", () => {
-    playMusicByArray(listNum)
 
-    ////////play and pause button///
-    playBtn.addEventListener("click", (e) => {
+playMusicByArray(listNum)
+
+////////play and pause button///
+playBtn.addEventListener("click", (e) => {
+    if (playBtn.className == "bi bi-pause-fill") {
+        audio.pause()
+        playBtn.className = "bi bi-play-fill"
+    } else {
+        audio.play()
+        playBtn.className = "bi bi-pause-fill"
+    }
+})
+////////
+//////keyboard logic////
+window.addEventListener("keydown", (e) => {
+    if (e.code == "Space") {
         if (playBtn.className == "bi bi-pause-fill") {
             audio.pause()
             playBtn.className = "bi bi-play-fill"
@@ -80,150 +90,140 @@ window.addEventListener("load", () => {
             audio.play()
             playBtn.className = "bi bi-pause-fill"
         }
-    })
-    ////////
-    //////keyboard logic////
-    window.addEventListener("keydown", (e) => {
-        if (e.code == "Space") {
-            if (playBtn.className == "bi bi-pause-fill") {
-                audio.pause()
-                playBtn.className = "bi bi-play-fill"
-            } else {
-                audio.play()
-                playBtn.className = "bi bi-pause-fill"
-            }
-        }
-        if (e.code == "ArrowLeft") {
-            listNum--
-            if (listNum < 0) listNum = 5
-            playMusicByArray(listNum)
-            playBtn.className = "bi bi-pause-fill"
-        }
-        if (e.code == "ArrowRight") {
-            listNum++
-            if (listNum > musicList.length - 1) listNum = 0
-            playMusicByArray(listNum)
-            playBtn.className = "bi bi-pause-fill"
-        }
-
-
-    })
-    /////////////////////////////
-    ///////next and privious  button
-    nextBtn.addEventListener("click", (e) => {
-        listNum++
-        if (listNum > musicList.length - 1) listNum = 0
-        playMusicByArray(listNum)
-        playBtn.className = "bi bi-pause-fill"
-
-    })
-    previousBtn.addEventListener("click", (e) => {
+    }
+    if (e.code == "ArrowLeft") {
         listNum--
         if (listNum < 0) listNum = 5
         playMusicByArray(listNum)
         playBtn.className = "bi bi-pause-fill"
-
-    })
-    //////////////
-    ////////////////play line ///////////////
-
-
-    playLine.addEventListener("click", (e) => {
-        let leftElement = playLine.getBoundingClientRect().left
-        let pxLeft = e.clientX - leftElement
-        let resulrPersent = (100 * pxLeft) / playLine.clientWidth
-        // let resulrPersentSec = 100 * 
-        playLineDot.style.left = resulrPersent + "%"
-        audio.currentTime = (resulrPersent * audio.duration) / 100
-        ///////////////////////
-
-        playLineFull.style.width = resulrPersent + "%"
-
-    })
-
-
-    ////////////////////
-    /////////////playLineDot//////////////
-
-
-
-    //////////////////
-    /////////setInterval time////////
-    let sec = 0
-    setInterval(() => {
-        if (audio.currentTime >= audio.duration - 2) {
-            listNum++
-            if (listNum > musicList.length - 1) listNum = 0;
-            playMusicByArray(listNum)
-
-
-        };
-
-        let result = (100 * audio.currentTime) / audio.duration
-        playLineDot.style.left = result + "%"
-        playLineFull.style.width = result + "%"
-        let second = Math.floor(audio.currentTime)
-        let endSec = Math.floor(audio.duration)
-        let endResMin = Math.floor((endSec / 60) - ((second / 60)))
-        let endResSec = (endSec - second) % 60
-
-        ////start/////
-        if (String(Math.floor(second / 60)).length < 2) {
-            startTimeList[0].innerHTML = "0" + Math.floor(second / 60)
-        } else {
-            startTimeList[0].innerHTML = Math.floor(second / 60)
-        }
-
-        if (String(Math.floor(second % 60)).length < 2) {
-            startTimeList[2].innerHTML = "0" + Math.floor(second % 60)
-        } else {
-            startTimeList[2].innerHTML = Math.floor(second % 60)
-        }
-        //////
-        ///////end///////
-        if (isNaN(endResMin)) {
-            endResMin = 0
-        } else {
-            if (String(endResMin).length < 2) {
-                endTimeList[0].innerHTML = "0" + endResMin
-            } else {
-                endTimeList[0].innerHTML = endResMin
-            }
-        }
-
-
-        // if (endResSec == 60) {
-        //     endResSec = 0
-        // }
-        if (isNaN(endResSec)) {
-            endResSec = 0
-
-        } else {
-            if (String(endResSec).length < 2) {
-                endTimeList[2].innerHTML = "0" + endResSec
-            } else {
-                endTimeList[2].innerHTML = endResSec
-            }
-        }
-
-        //////////////
-    }, 1000)
-
-
-
-
-    /////////////
-    //////////////////////array play function//////
-    function playMusicByArray(listNum) {
-        img.src = musicList[listNum].imgUrl
-        audio.src = musicList[listNum].MusicUrl
-        musicName.innerHTML = musicList[listNum].musicName
-        singerName.innerHTML = musicList[listNum].singer
-        beforeBg.style.backgroundImage = `url(${musicList[listNum].imgUrl})`
-        body.style.backgroundColor = musicList[listNum].bgColor
-        audio.play()
     }
+    if (e.code == "ArrowRight") {
+        listNum++
+        if (listNum > musicList.length - 1) listNum = 0
+        playMusicByArray(listNum)
+        playBtn.className = "bi bi-pause-fill"
+    }
+
+
+})
+/////////////////////////////
+///////next and privious  button
+nextBtn.addEventListener("click", (e) => {
+    listNum++
+    if (listNum > musicList.length - 1) listNum = 0
+    playMusicByArray(listNum)
+    playBtn.className = "bi bi-pause-fill"
+
+})
+previousBtn.addEventListener("click", (e) => {
+    listNum--
+    if (listNum < 0) listNum = 5
+    playMusicByArray(listNum)
+    playBtn.className = "bi bi-pause-fill"
+
+})
+//////////////
+////////////////play line ///////////////
+
+
+playLine.addEventListener("click", (e) => {
+    let leftElement = playLine.getBoundingClientRect().left
+    let pxLeft = e.clientX - leftElement
+    let resulrPersent = (100 * pxLeft) / playLine.clientWidth
+    // let resulrPersentSec = 100 * 
+    playLineDot.style.left = resulrPersent + "%"
+    audio.currentTime = (resulrPersent * audio.duration) / 100
+    ///////////////////////
+
+    playLineFull.style.width = resulrPersent + "%"
+    console.log(resulrPersent + "%");
+
 })
 
 
+////////////////////
+/////////////playLineDot//////////////
 
+
+
+//////////////////
+/////////setInterval time////////
+let sec = 0
+setInterval(() => {
+    if (audio.currentTime >= audio.duration - 2) {
+        listNum++
+        if (listNum > musicList.length - 1) listNum = 0;
+        playMusicByArray(listNum)
+        console.log("golog");
+
+
+    };
+
+    let result = (100 * audio.currentTime) / audio.duration
+    playLineDot.style.left = result + "%"
+    playLineFull.style.width = result + "%"
+    let second = Math.floor(audio.currentTime)
+    let endSec = Math.floor(audio.duration)
+    let endResMin = Math.floor((endSec / 60) - ((second / 60)))
+    let endResSec = (endSec - second) % 60
+
+    ////start/////
+    if (String(Math.floor(second / 60)).length < 2) {
+        startTimeList[0].innerHTML = "0" + Math.floor(second / 60)
+    } else {
+        startTimeList[0].innerHTML = Math.floor(second / 60)
+    }
+
+    if (String(Math.floor(second % 60)).length < 2) {
+        startTimeList[2].innerHTML = "0" + Math.floor(second % 60)
+    } else {
+        startTimeList[2].innerHTML = Math.floor(second % 60)
+    }
+    //////
+    ///////end///////
+    if (isNaN(endResMin)) {
+        endResMin = 0
+    } else {
+        if (String(endResMin).length < 2) {
+            endTimeList[0].innerHTML = "0" + endResMin
+        } else {
+            endTimeList[0].innerHTML = endResMin
+        }
+    }
+
+
+    // if (endResSec == 60) {
+    //     endResSec = 0
+    // }
+    if (isNaN(endResSec)) {
+        endResSec = 0
+
+    } else {
+        if (String(endResSec).length < 2) {
+            endTimeList[2].innerHTML = "0" + endResSec
+        } else {
+            endTimeList[2].innerHTML = endResSec
+        }
+    }
+    console.log(endResSec);
+
+    //////////////
+}, 1000)
+
+
+
+
+/////////////
+//////////////////////array play function//////
+function playMusicByArray(listNum) {
+    img.src = musicList[listNum].imgUrl
+    beforeBg.style.backgroundImage = `url(${musicList[listNum].imgUrl})`
+    body.style.backgroundColor = musicList[listNum].bgColor
+    musicName.innerHTML = musicList[listNum].musicName
+    singerName.innerHTML = musicList[listNum].singer
+    ////////////////////////////////////////////
+    audio.src = musicList[listNum].MusicUrl
+    audio.load()
+    ///////////////////////////
+    audio.play()
+}
